12f5413b1b477f0edf92fe40590bf08ef6cd6ffc
abeertech01 <abeer.technology@gmail.com>
2023-07-18 17:14:20 +0600

    workspace design developed

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,9 +1,15 @@
 import Topbar from "@/components/Topbar/Topbar"
+import Workspace from "@/components/Workspace/Workspace"
 import React from "react"
 
 type ProblemPageProps = {}
 
 const ProblemPage: React.FC<ProblemPageProps> = () => {
-  return <Topbar problemPage={true} />
+  return (
+    <div>
+      <Topbar problemPage />
+      <Workspace />
+    </div>
+  )
 }
 export default ProblemPage
