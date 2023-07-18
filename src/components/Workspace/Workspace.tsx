7f25fe9ac54a08d3105e68faf258fba216de8645
abeertech01 <abeer.technology@gmail.com>
2023-07-18 17:14:20 +0600

    workspace design developed

---
+++ src/components/Workspace/Workspace.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Split from "react-split"
+import ProblemDescription from "./ProblemDescription.tsx/ProblemDescription"
+import Playground from "./Playground/Playground"
+
+type WorkspaceProps = {}
+
+const Workspace: React.FC<WorkspaceProps> = () => {
+  return (
+    <Split className="split">
+      <ProblemDescription />
+      <Playground />
+    </Split>
+  )
+}
+export default Workspace
