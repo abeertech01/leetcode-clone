07d0d2017b29b1a8e19aaa278ea40a37f9437e53
abeertech01 <abeer.technology@gmail.com>
2023-07-30 01:58:18 +0600

    everything is done

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react"
-import { problems } from "@/mockProblems/problems"
 import { BsCheckCircle } from "react-icons/bs"
 import { AiFillYoutube } from "react-icons/ai"
 import Link from "next/link"
 import { IoClose } from "react-icons/io5"
 import YouTube from "react-youtube"
-import { collection, getDocs, orderBy, query } from "firebase/firestore"
-import { firestore } from "@/firebase/firebase"
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+} from "firebase/firestore"
+import { auth, firestore } from "@/firebase/firebase"
 import { DBProblem } from "@/utils/types/problem"
+import { useAuthState } from "react-firebase-hooks/auth"
 
 type ProblemsTableProps = {
   setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>
@@ -21,6 +28,7 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({
     videoId: "",
   })
   const problems = useGetProblems(setLoadingProblems)
+  const solvedProblems = useGetSolvedProblems()
   const closeModal = () => {
     setYouTubePlayer({ isOpen: false, videoId: "" })
   }
@@ -50,7 +58,9 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({
               key={problem.id}
             >
               <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
-                <BsCheckCircle fontSize={"18"} width="18" />
+                {solvedProblems.includes(problem.id) && (
+                  <BsCheckCircle fontSize={"18"} width="18" />
+                )}
               </th>
               <td className="px-6 py-4">
                 {problem.link ? (
@@ -146,4 +156,24 @@ function useGetProblems(
   }, [setLoadingProblems])
 
   return problems
+}
+
+function useGetSolvedProblems() {
+  const [solvedProblems, setSolvedProblems] = useState<string[]>([])
+  const [user] = useAuthState(auth)
+
+  useEffect(() => {
+    const getSolvedProblems = async () => {
+      const userRef = doc(firestore, "users", user!.uid)
+      const userDoc = await getDoc(userRef)
+
+      if (userDoc.exists()) {
+        setSolvedProblems(userDoc.data().solvedProblems)
+      }
+    }
+    if (user) getSolvedProblems()
+    if (!user) setSolvedProblems([])
+  }, [user])
+
+  return solvedProblems
 }
